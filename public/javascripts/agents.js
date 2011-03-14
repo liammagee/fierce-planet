686af507b8efa864b5d4bb5d563f645edc21dc2d
doclm <liam.magee@gmail.com>
2011-03-14 15:34:51 +1100

    health and speed adjustments, based on good patches

--- public/javascripts/agents.js
+++ public/javascripts/agents.js
@@ -15,10 +15,13 @@ var cellWidth = 400 / worldSize;
 var pieceWidth = cellWidth * 0.5;
 var agents;
 var patches = new Array();
+var activePatches = new Array();
 var cells = new Hash();
 var counter = 0;
 var counterLoops = 0;
 var MOVE_INCREMENTS = 5;
+var INITIAL_HEALTH = 100;
+var MOVE_HEALTH_COST = -5;
 
 /* Agent class definition */
 function Agent(agentType, color, x, y) {
@@ -41,7 +44,10 @@ function Agent(agentType, color, x, y) {
         this._history.push([tmpX, tmpY])
     }
     this._delay = 0;
+    this._wanderX = 0;
+    this._wanderY = 0;
     this._speed = MOVE_INCREMENTS;
+    this._health = INITIAL_HEALTH;
 }
 Agent.prototype.getPosition = function() { return [this._x, this._y]; }
 Agent.prototype.setPosition = function(x, y) { this._history.push([this._x, this._y]); this._x =x; this._y = y; }
@@ -54,11 +60,37 @@ Agent.prototype.getType = function() { return this._agentType;}
 Agent.prototype.getColor = function() { return this._color;}
 Agent.prototype.getDelay = function() { return this._delay; }
 Agent.prototype.setDelay = function(delay) { this._delay = delay; }
+Agent.prototype.getHealth = function() { return this._health; }
+Agent.prototype.setHealth = function(health) { this._health = health; }
+Agent.prototype.adjustHealth = function(adjustment) {
+    var newHealth = this._health + adjustment;
+    if (newHealth > 0)
+        this._health = newHealth;
+    else
+        this._health = 0;
+}
+Agent.prototype.getWanderX = function() { return this._wanderX; }
+Agent.prototype.getWanderY = function() { return this._wanderY; }
+Agent.prototype.adjustWander = function() {
+    var wx = this._wanderX;
+    var wy = this._wanderY;
+    var limit = cellWidth / 2 - pieceWidth / 2;
+    var rx = Math.floor(Math.random() * 3 - 1);
+    var ry = Math.floor(Math.random() * 3 - 1);
+    wx = wx + rx;
+    wy = wy + ry;
+
+    if (limit - Math.abs(wx) > 0)
+        this._wanderX = wx;
+
+    if (limit - Math.abs(wy) > 0)
+        this._wanderY = wy;
+}
 Agent.prototype.getSpeed = function() { return this._speed; }
 Agent.prototype.setSpeed = function(speed) { this._speed = speed; }
 /* Change the speed, but sparingly as the speed moves away from the standard speed: MOVE_INCREMENTS */
 Agent.prototype.adjustSpeed = function() {
-    var tmpSpeed = 0;
+    var tmpSpeed = this._speed;
     var variance = this._speed - MOVE_INCREMENTS;
 
     // Makes movement away from MOVE_INCREMENTS very unlikely
@@ -66,21 +98,26 @@ Agent.prototype.adjustSpeed = function() {
     // Makes movement away from MOVE_INCREMENTS unlikely
 //    var prob = Math.pow(Math.abs(variance), 2);
     // Makes movement away from MOVE_INCREMENTS moderately likely
-//    var prob = Math.abs(variance);
+    var prob = Math.abs(variance);
     // Makes movement away from MOVE_INCREMENTS an even chance
-    var prob = 1;
+//    var prob = 1;
 
-    var r = Math.floor(Math.random() * 2 * prob - 1);
+    var r = Math.floor(Math.random() * 3 * prob - 1);
     // Set the speed to above, equal or below the current speed
     var change = (r < 0 ? -1 : (r > 0 ? 1 : 0));
     // Change direction if the speed is already negative
-    change = (variance < 0 ? -change : change);
+    change = (variance > 0 ? -change : change);
 
     // Add a multiplier to the change
-    var multiplier = Math.ceil(Math.random() * 3);
-//    var multiplier = 1;
+//    var multiplier = Math.ceil(Math.random() * 3);
+    var multiplier = 1;
 
-    this._speed = this._speed + change * multiplier;
+    var s = this._speed;
+
+    tmpSpeed = this._speed + change * multiplier;
+
+    if (tmpSpeed > 0)
+        this._speed = tmpSpeed;
 }
 
 
@@ -88,11 +125,15 @@ Agent.prototype.adjustSpeed = function() {
 function Patch(patchType, color) {
     this._patchType = patchType;
     this._color = color;
+    this._totalYield = 0;
+    this._perAgentYield = 0;
 }
 function Patch(patchType, color, x, y) {
     this._patchType = patchType;
     this._color = color;
     this.setPosition(x, y);
+    this._totalYield = 0;
+    this._perAgentYield = 0;
 }
 Patch.prototype.getType = function() { return this._patchType;}
 Patch.prototype.getColor = function() { return this._color;}
@@ -102,7 +143,17 @@ Patch.prototype.getX = function() { return this._x; }
 Patch.prototype.setX = function(x) { this._x = x; }
 Patch.prototype.getY = function() { return this._y; }
 Patch.prototype.setY = function(y) { this._y = y; }
-
+Patch.prototype.getTotalYield = function() { return this._totalYield; }
+Patch.prototype.setTotalYield = function(totalYield) { this._totalYield = totalYield; }
+Patch.prototype.getPerAgentYield = function() { return this._perAgentYield; }
+Patch.prototype.setPerAgentYield = function(perAgentYield) { this._perAgentYield = perAgentYield; }
+Patch.prototype.provideYield = function(agent) {
+    if (this._totalYield > this._perAgentYield) {
+        agent.adjustHealth(this._perAgentYield);
+        agent.setSpeed(this._perAgentYield);
+        this._totalYield -= this._perAgentYield;
+    }
+}
 
 
 initWorld();
@@ -111,7 +162,10 @@ function initWorld() {
     cellWidth = 400 / worldSize;
     pieceWidth = cellWidth * 0.5;
     patches = new Array();
+    activePatches = new Array();
+    agents = new Array();
     cells = new Hash();
+
     setupWorld1();
     //agents = randomAgents(numAgents, worldSize);
 }
@@ -178,19 +232,24 @@ function dropItem(e) {
 
     var patchType = document.getElementById(e.dataTransfer.getData('Text')).id;
     var c = "0f0";
+    var totalYield = 0, perAgentYield = 0;
     if (patchType == 'eco') {
         c = "f00";
     }
     else if (patchType == 'env') {
         c = "0f0";
+        totalYield = 100;
+        perAgentYield = 10;
     }
     else if (patchType == 'soc') {
         c = "00f";
     }
 
-    var patch = new Patch(patchType, c);
-    patch.setPosition(cellX / cellWidth, cellY / cellWidth);
+    var patch = new Patch(patchType, c, cellX / cellWidth, cellY / cellWidth);
+    patch.setTotalYield(totalYield);
+    patch.setPerAgentYield(perAgentYield);
     patches.push(patch);
+    activePatches.push(patch);
     cells.set([patch.getX(), patch.getY()], patch);
 
     drawPatch(patch);
@@ -270,18 +329,21 @@ function findPosition(x, y, withNoRepeat, withNoCollision, lastX, lastY) {
         }
         existingDirections.push(dir);
 
+        var offScreen1 = 0;
+        var offScreen2 = worldSize - 1;
+        var offset = 1;
         switch (dir) {
             case 0:
-                (newX == 0) ? newX = worldSize - 1 : newX = newX - 1;
+                (newX == offScreen1) ? newX = offScreen2 : newX = newX - offset;
                 break;
             case 1:
-                (newX == worldSize - 1) ? newX = 0 : newX = newX + 1;
+                (newX == offScreen2) ? newX = offScreen1 : newX = newX + offset;
                 break;
             case 2:
-                (newY == 0) ? newY = worldSize - 1 : newY = newY - 1;
+                (newY == offScreen1) ? newY = offScreen2 : newY = newY - offset;
                 break;
             case 3:
-                (newY == worldSize - 1) ? newY = 0 : newY = newY + 1;
+                (newY == offScreen2) ? newY = offScreen1 : newY = newY + offset;
                 break;
         }
         if (withNoRepeat == true && lastX == newX && lastY == newY) {
@@ -292,22 +354,26 @@ function findPosition(x, y, withNoRepeat, withNoCollision, lastX, lastY) {
         }
     }
     // Allow for movement off-screen, if no other option is available
-    if (x == worldSize - 1 || x == 0 || y == worldSize - 1 || y == 0) {
-        if (x == worldSize - 1) {
-            x = worldSize;
+    if (x == offScreen2 || x == offScreen1 || y == offScreen2 || y == offScreen1) {
+        if (x == offScreen2) {
+            x = offScreen2 + 1;
         }
-        else if (x == 0) {
-            x = -1;
+        else if (x == offScreen1) {
+            x = offScreen1 - 1;
         }
-        else if (y == worldSize - 1) {
-            y = worldSize;
+        else if (y == offScreen2) {
+            y = offScreen2 + 1;
         }
-        else if (y == 0) {
-            y = worldSize;
+        else if (y == offScreen1) {
+            y = offScreen1 - 1;
         }
     }
 
     return [x, y];
+}
+
+function checkCollision(x, y) {
+    return cells.get([x, y]) == undefined;
 }
 
 function moveAgentsRandomly() {
@@ -376,32 +442,45 @@ function drawAgents() {
     if (counter > 0) {
         for (var i = 0; i < agents.length; i+= 1) {
             var agent = agents[i];
-            var lastX = agent.lastPosition()[0] * cellWidth + (cellWidth / 2);
-            var lastY = agent.lastPosition()[1] * cellWidth + (cellWidth / 2);
-            var x = agent.getPosition()[0] * cellWidth + (cellWidth / 2);
-            var y = agent.getPosition()[1] * cellWidth + (cellWidth / 2);
+            var lastX = agent.lastPosition()[0];
+            var lastY = agent.lastPosition()[1];
+            var x = agent.getPosition()[0];
+            var y = agent.getPosition()[1];
+            var wx = agent.getWanderX();
+            var wy = agent.getWanderY();
             var speed = agent.getSpeed();
             var increment = (speed - (counter - 1 - agent.getDelay()) % speed) / speed;
-            if (increment == 0)
-                increment = 1;
-            var offsetX = parseInt((x - lastX) * (increment));
-            var offsetY = parseInt((y - lastY) * (increment));
-            var intX = x - offsetX - (cellWidth / 2) + 1;
-            var intY = y - offsetY - (cellWidth / 2) + 1;
-            var radius = (pieceWidth / 2);
+//            if (increment == 0)
+//                increment = 1;
+            var offsetX = (x - lastX) * (increment);
+            var offsetY = (y - lastY) * (increment);
+            var intX = (x - offsetX) * cellWidth + wx + 1;
+            var intY = (y - offsetY) * cellWidth + wy + 1;
 
             ctx.clearRect(intX, intY, cellWidth - 2, cellWidth - 2);
         }
     }
 
+    var deadAgents = new Array();
     for (var i = 0; i < agents.length; i+= 1) {
         var agent = agents[i];
         var speed = agent.getSpeed();
         if (counter >= agent.getDelay() && (counter - agent.getDelay()) % speed == 0) {
             moveAgent(agent, true, false);
             agent.adjustSpeed();
+            agent.adjustWander();
+            agent.adjustHealth(MOVE_HEALTH_COST);
+            if (agent.getHealth() == 0)
+                deadAgents.push(i);
+
+            // Hook for detecting 'active' patches
+            processNeighbouringPatches(agent);
         }
     }
+    for (var i = deadAgents.length - 1; i >= 0; i-= 1) {
+        agents.splice(deadAgents[i]);
+    }
+
 
 //    if (counter % 5 == 0)
 //        moveAgents(true, false);
@@ -415,21 +494,22 @@ function drawAgents() {
     //drawPatches();
 
 
-    // Hook for detecting 'active' patches
-    processNeighbouringPatches();
-
     for (var i = 0; i < agents.length; i+= 1) {
         var agent = agents[i];
-        var lastX = agent.lastPosition()[0] * cellWidth + (cellWidth / 2);
-        var lastY = agent.lastPosition()[1] * cellWidth + (cellWidth / 2);
-        var x = agent.getPosition()[0] * cellWidth + (cellWidth / 2);
-        var y = agent.getPosition()[1] * cellWidth + (cellWidth / 2);
+        var lastX = agent.lastPosition()[0];
+        var lastY = agent.lastPosition()[1];
+        var x = agent.getPosition()[0];
+        var y = agent.getPosition()[1];
+        var wx = agent.getWanderX();
+        var wy = agent.getWanderY();
         var speed = agent.getSpeed();
         var increment = (speed - (counter - agent.getDelay()) % speed) / speed;
-        var offsetX = parseInt((x - lastX) * (increment));
-        var offsetY = parseInt((y - lastY) * (increment));
-        var intX = x - offsetX;
-        var intY = y - offsetY;
+        var offsetX = (x - lastX) * (increment);
+        var offsetY = (y - lastY) * (increment);
+        var intX = (x - offsetX) * cellWidth + wx + cellWidth / 2;
+        var intY = (y - offsetY) * cellWidth + wy + cellWidth / 2;
+
+
         var radius = (pieceWidth / 2);
 
         ctx.beginPath();
@@ -437,24 +517,31 @@ function drawAgents() {
         ctx.closePath();
         ctx.strokeStyle = "#ccc";
         ctx.stroke();
-        ctx.fillStyle = "#" + agent.getColor();
+        var health = agent.getHealth();
+        var hOffset = Math.floor(health / 10);
+        hOffset = (hOffset > 10 ? 10 : hOffset);
+        var c = agent.getColor().toString();
+        var r = parseInt(c.slice(0, 1), 16);
+        var g = parseInt(c.slice(1, 2), 16);
+        var b = parseInt(c.slice(2, 3), 16);
+        var newColor = (r - hOffset).toString(16) + (g - hOffset).toString(16) + (b - hOffset).toString(16);
+        ctx.fillStyle = "#" + newColor;
         ctx.fill();
+        ctx.fillStyle = "#0f0";
+        ctx.fillText(agent.getHealth(), intX, intY);
     }
 }
 
 // NOTE: very inefficient
-function processNeighbouringPatches() {
-    for (var i = 0; i < agents.length; i+= 1) {
-        var agent = agents[i];
-        var x = agent.getX();
-        var y = agent.getY();
-        for (var j = 0; j < patches.length; j++) {
-            var p = patches[j];
-            var px = p.getX();
-            var py = p.getY();
-            if (Math.abs(px - x) <= 1 && Math.abs(py - y) <= 1 && p.getType() == 'env') {
-                agent.setSpeed(10);
-            }
+function processNeighbouringPatches(agent) {
+    var x = agent.getX();
+    var y = agent.getY();
+    for (var j = 0; j < activePatches.length; j++) {
+        var p = activePatches[j];
+        var px = p.getX();
+        var py = p.getY();
+        if (Math.abs(px - x) <= 1 && Math.abs(py - y) <= 1 && p.getType() == 'env') {
+            p.provideYield(agent);
         }
     }
 }
@@ -507,10 +594,10 @@ function randomAgents(number, limit) {
     return agents;
 }
 
-function presetAgents(number, x, y) {
+function presetAgents(number, cellX, cellY) {
     var agents = new Array();
     for (var i = 0; i < number; i ++) {
-        var agent = new Agent("basic", "888", x, y);
+        var agent = new Agent("basic", "ccc", cellX, cellY);
         var delay = parseInt(Math.random() * MOVE_INCREMENTS * 5);
         agent.setDelay(delay);
 //        agent.setDelay(2);
