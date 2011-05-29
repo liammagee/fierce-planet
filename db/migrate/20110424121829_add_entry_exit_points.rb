1324a54dddb8901c25f4e0b249f106a506de142a
doclm <liam.magee@gmail.com>
2011-05-30 08:43:04 +1000

    Adding improved level editing

--- db/migrate/20110424121829_add_entry_exit_points.rb
+++ db/migrate/20110424121829_add_entry_exit_points.rb
@@ -1,9 +1,11 @@
-class AddSoundSrcToCustomLevels < ActiveRecord::Migration
+class AddEntryExitPoints < ActiveRecord::Migration
   def self.up
-    add_column :levels, :sound_src, :string
+    add_column :levels, :entry_points, :string
+    add_column :levels, :exit_points, :string
   end
 
   def self.down
-    remove_column :levels, :sound_src
+    remove_column :levels, :entry_points
+    remove_column :levels, :exit_points
   end
 end
