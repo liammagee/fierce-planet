7cdf4597ef7559b18f64a303c7c7ca6f6fd05327
doclm <liam.magee@gmail.com>
2011-07-03 00:08:33 +1000

    Added basic Facebook, Google login support

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,5 @@ class User < ActiveRecord::Base
      else # Create a user with a stub password.
        User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
      end
+  end
 end
