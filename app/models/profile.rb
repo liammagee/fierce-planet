099cb392f87142d4134cfd803db82f650b3b8ff7
doclm <liam.magee@gmail.com>
2011-07-03 11:19:17 +1000

    Updated profile defaults

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,13 +2,15 @@ class Profile < ActiveRecord::Base
   attr_accessor :previous_level_score, :expired_agent_count, :saved_agent_count, :saved_agent_this_wave_count, :waves_survived, :resource_count, :resources_in_store, :resources_spent, :resourceStatsCount
   
   belongs_to :user
+  after_initialize :init
+  before_save :serialise_capabilities
 
-#  has_many :capabilities
-  def after_initialize
-    self.capabilities = self.capabilities.split(',') if self.capabilities
+  def init
+    self.profile_class ||= 'Novice'
+    self.capabilities = self.capabilities ? self.capabilities.split(',') : ['farm', 'water', 'clinic']
   end
 
-  def before_save
+  def serialise_capabilities
     self.capabilities = self.capabilities.join(',') if self.capabilities
   end
 end
