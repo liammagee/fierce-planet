83103520a957463926e373c5f994caf336e175d8
doclm <liam.magee@gmail.com>
2011-07-05 19:58:54 +1000

    Adding new icons, background

--- public/javascripts/agents/fp-data/resource_types.js
+++ public/javascripts/agents/fp-data/resource_types.js
@@ -1,27 +1,27 @@
 
 
 // Economic resources
-FiercePlanet.FARM_RESOURCE_TYPE = new ResourceType("Farm", "farm", "/images/resources/farm.png", 10, 20, 100, 20);
-FiercePlanet.SHOP_RESOURCE_TYPE = new ResourceType("Shop", "shop", "/images/resources/shop.png", 15, 25, 150, 30);
-FiercePlanet.BANK_RESOURCE_TYPE = new ResourceType("Bank", "bank", "/images/resources/bank.png", 20, 30, 200, 50);
-FiercePlanet.FACTORY_RESOURCE_TYPE = new ResourceType("Factory", "factory", "/images/resources/factory.png", 25, 40, 250, 70);
-FiercePlanet.STOCKMARKET_RESOURCE_TYPE = new ResourceType("Stockmarket", "stockmarket", "/images/resources/stockmarket.png", 30, 50, 300, 90);
+FiercePlanet.FARM_RESOURCE_TYPE = new ResourceType("Farm", "farm", "/images/icons/farm.png", 10, 20, 100, 20);
+FiercePlanet.SHOP_RESOURCE_TYPE = new ResourceType("Shop", "shop", "/images/icons/shop.png", 15, 25, 150, 30);
+FiercePlanet.BANK_RESOURCE_TYPE = new ResourceType("Bank", "bank", "/images/icons/bank.png", 20, 30, 200, 50);
+FiercePlanet.FACTORY_RESOURCE_TYPE = new ResourceType("Factory", "factory", "/images/icons/factory.png", 25, 40, 250, 70);
+FiercePlanet.STOCKMARKET_RESOURCE_TYPE = new ResourceType("Stockmarket", "stockmarket", "/images/icons/stockmarket.png", 30, 50, 300, 90);
 
 
 // Environmental resources
-FiercePlanet.FRESH_WATER_RESOURCE_TYPE = new ResourceType("Fresh Water", "water", "/images/resources/water.png", 10, 20, 100, 10);
-FiercePlanet.WILDLIFE_PARK_RESOURCE_TYPE = new ResourceType("Wildlife Park", "park", "/images/resources/park.png", 15, 25, 150, 15);
-FiercePlanet.CLEAN_AIR_RESOURCE_TYPE = new ResourceType("Clear Air", "air", "/images/resources/air.png", 20, 30, 200, 25);
-FiercePlanet.GREEN_ENERGY_RESOURCE_TYPE = new ResourceType("Green Energy", "energy", "/images/resources/energy.png", 25, 40, 250, 35);
-FiercePlanet.BIODIVERSITY_RESOURCE_TYPE = new ResourceType("Biodiversity", "biodiversity", "/images/resources/biodiversity.png", 30, 300, 100, 45);
+FiercePlanet.FRESH_WATER_RESOURCE_TYPE = new ResourceType("Fresh Water", "water", "/images/icons/water.png", 10, 20, 100, 10);
+FiercePlanet.WILDLIFE_PARK_RESOURCE_TYPE = new ResourceType("Wildlife Park", "park", "/images/icons/park.png", 15, 25, 150, 15);
+FiercePlanet.CLEAN_AIR_RESOURCE_TYPE = new ResourceType("Clear Air", "air", "/images/icons/air.png", 20, 30, 200, 25);
+FiercePlanet.GREEN_ENERGY_RESOURCE_TYPE = new ResourceType("Green Energy", "energy", "/images/icons/energy.png", 25, 40, 250, 35);
+FiercePlanet.BIODIVERSITY_RESOURCE_TYPE = new ResourceType("Biodiversity", "biodiversity", "/images/icons/biodiversity.png", 30, 300, 100, 45);
 
 
 // Social resources
-FiercePlanet.CLINIC_RESOURCE_TYPE = new ResourceType("Clinic", "clinic", "/images/resources/clinic.png", 10, 20, 100, 5);
-FiercePlanet.SCHOOL_RESOURCE_TYPE = new ResourceType("School", "school", "/images/resources/school.png", 15, 25, 150, 8);
-FiercePlanet.LEGAL_SYSTEM_RESOURCE_TYPE = new ResourceType("Legal System", "legal", "/images/resources/legal.png", 20, 30, 200, 15);
-FiercePlanet.DEMOCRACY_RESOURCE_TYPE = new ResourceType("Democracy", "democracy", "/images/resources/democracy.png", 25, 40, 250, 20);
-FiercePlanet.FESTIVAL_RESOURCE_TYPE = new ResourceType("Festival", "festival", "/images/resources/festival.png", 30, 50, 300, 25);
+FiercePlanet.CLINIC_RESOURCE_TYPE = new ResourceType("Clinic", "clinic", "/images/icons/clinic.png", 10, 20, 100, 5);
+FiercePlanet.SCHOOL_RESOURCE_TYPE = new ResourceType("School", "school", "/images/icons/school.png", 15, 25, 150, 8);
+FiercePlanet.LEGAL_SYSTEM_RESOURCE_TYPE = new ResourceType("Legal System", "legal", "/images/icons/legal.png", 20, 30, 200, 15);
+FiercePlanet.DEMOCRACY_RESOURCE_TYPE = new ResourceType("Democracy", "democracy", "/images/icons/democracy.png", 25, 40, 250, 20);
+FiercePlanet.FESTIVAL_RESOURCE_TYPE = new ResourceType("Festival", "festival", "/images/icons/festival.png", 30, 50, 300, 25);
 
 
 /**
